75dc8ffcef47f0abc824a83292b77d652d92757b
KAA0070209 <m.azkanabhan07@gmail.com>
2026-08-13 21:29:27 +0700

    Fixing bug

---
+++ alembic/versions/bcdeb2354033_compat_missing_deployed_revision.py
@@ -0,0 +1,28 @@
+"""compat missing deployed revision
+
+Revision ID: bcdeb2354033
+Revises: b2c3d4e5f6a7
+Create Date: 2026-08-13 21:35:00.000000
+
+"""
+from typing import Sequence, Union
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'bcdeb2354033'
+down_revision: Union[str, Sequence[str], None] = 'b2c3d4e5f6a7'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    # This revision was already stamped in production by a migration file that
+    # is no longer present in the repository. Keep it as a no-op bridge so
+    # Alembic can continue from that deployed database state.
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
--- alembic/versions/d4e5f6a7b8c9_recreate_transactions_table.py
+++ alembic/versions/d4e5f6a7b8c9_recreate_transactions_table.py
@@ -14,7 +14,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = 'd4e5f6a7b8c9'
-down_revision: Union[str, Sequence[str], None] = 'b2c3d4e5f6a7'
+down_revision: Union[str, Sequence[str], None] = 'bcdeb2354033'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
