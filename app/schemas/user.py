e75b1316455b0d216f8ec11f0cac9b7f04f4de67
KAA0070209 <m.azkanabhan07@gmail.com>
2026-08-13 18:43:21 +0700

    Fixing bug

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Optional
+from typing import List, Optional
 from pydantic import BaseModel, EmailStr, Field, model_validator
 from app.schemas.role import Role
 
