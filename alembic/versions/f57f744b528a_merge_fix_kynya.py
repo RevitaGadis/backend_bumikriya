fe2818cc38e0a480f3131d7e2fbb9d769a4716cd
KAA0070209 <m.azkanabhan07@gmail.com>
2026-08-16 07:39:52 +0700

    fix

--- alembic/versions/f57f744b528a_merge_fix_kynya.py
+++ alembic/versions/f57f744b528a_merge_fix_kynya.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'f57f744b528a'
-down_revision: Union[str, Sequence[str], None] = ('b8c9d1e2f3a4', 'e1f2a3b4c5d6')
+down_revision: Union[str, Sequence[str], None] = ('b8c9d1e2f3a4', 'bcdeb2354033')
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
