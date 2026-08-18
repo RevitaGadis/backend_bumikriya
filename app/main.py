46fbba0240b413ca296306105f96771a36620b4c
KAA0070209 <m.azkanabhan07@gmail.com>
2026-08-18 09:04:03 +0700

    fix final

--- app/main.py
+++ app/main.py
@@ -65,7 +65,8 @@ app.add_middleware(
 
 app.include_router(auth.router,        prefix="/api/v1/auth",        tags=["Auth"])
 app.include_router(checkout.router,    prefix="/api/v1/checkout",    tags=["Checkout"])
-app.include_router(home.router,        prefix="/api/v1/home",        tags=["Home"])
+app.include_router(search.router,        prefix="/api/v1/home",        tags=["Home"])
+app.include_router(search.router,        prefix="/search",            tags=["Search"])
 app.include_router(category.router,    prefix="/api/v1/categories",  tags=["Categories"])
 app.include_router(seller.router,      prefix="/api/v1/seller",      tags=["Seller"])
 app.include_router(user.router,        prefix="/api/v1/user",        tags=["User"])
